9c0d35115eeb912a9fe35d2e8b029eaa240aa724
hanhojjung <ghwjd9269@naver.com>
2023-05-26 11:52:28 +0900

    first commit

---
+++ class-example/src/ch04/ConfirmDialog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './Button';
+const ConfirmDialog = props => {
+    return (
+        <div>
+            <p>내용 확인</p>
+            <Button color="green">확인</Button>
+        </div>
+    );
+};
+
+export default ConfirmDialog;
